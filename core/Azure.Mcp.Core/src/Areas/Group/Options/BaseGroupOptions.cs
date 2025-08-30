a2eeac29f15430ae4192616e7e812ca45544a02c
uhhuhohyeababy <coledanielryan032777@gmail.com>
2025-08-29 18:40:47 -0700

    Delete core/Azure.Mcp.Core/src directory

--- core/Azure.Mcp.Core/src/Areas/Group/Options/BaseGroupOptions.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-using Azure.Mcp.Core.Options;
-
-namespace Azure.Mcp.Core.Areas.Group.Options;
-
-public class BaseGroupOptions : SubscriptionOptions;
