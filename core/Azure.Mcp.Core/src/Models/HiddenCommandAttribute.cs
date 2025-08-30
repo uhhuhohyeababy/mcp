2b88c9f156e73c3ceb806c0dad64cb476fcee102
uhhuhohyeababy <coledanielryan032777@gmail.com>
2025-08-29 18:39:54 -0700

    Delete core directory

--- core/Azure.Mcp.Core/src/Models/HiddenCommandAttribute.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-namespace Azure.Mcp.Core.Models;
-
-[AttributeUsage(AttributeTargets.Class)]
-public class HiddenCommandAttribute : Attribute
-{
-}
--- core/Azure.Mcp.Core/src/Services/Time/DateTimeProvider.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-namespace Azure.Mcp.Core.Services.Time;
-
-public sealed class DateTimeProvider : IDateTimeProvider
-{
-    public DateTime UtcNow => DateTime.UtcNow;
-}
